24137b10d29a69541cb9d8ab4bdf9f1b551e7c12
Stephane Mensah <1687551+waptik@users.noreply.github.com>
2023-12-20 10:30:08 +0000

    import `queues.ts` in `fresh.config.ts` instead of `main.ts`

--- fresh.config.ts
+++ fresh.config.ts
@@ -1,5 +1,7 @@
 import { defineConfig } from "$fresh/server.ts";
 import tailwind from "$fresh/plugins/tailwind.ts";
+import './queues.ts'
+
 
 export default defineConfig({
   plugins: [tailwind()],
